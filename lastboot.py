103675823a73f995597a9de11c226458afa6dba0
kennykwl <123275830+kennykwl@users.noreply.github.com>
2024-10-18 13:12:37 -0400

    Create lastboot.py

---
+++ lastboot.py
@@ -0,0 +1,25 @@
+import wmi
+import argparse
+from datetime import datetime
+
+def get_last_boot_time(computername):
+    try:
+        connection = wmi.WMI(computername=computer_name)
+        for os in connection.Win32_OperatingSystem():
+          last_boot = os.LastBootUpTime.split('.')[0]
+          boot_time = datetime.strptime(last_boot, '%Y%m%d%H%M%S')
+          formatted_time = boot_time.strftime('%Y-%m-%d %H:%M')
+
+          print(f"Computer Name: {os.CSName}')
+          print(f"Last Boot Time: {formatted_time}"
+    except Exception as e:
+          print(f"Failed to connect or retrieve data: {e}")
+
+if __name__ = "__main__":
+    parser = argparse.ArgumentParser(description="Get the last boot time of a remote Windows computer.")
+    parser.add_argument('--computername', required=True, help="The name of the remote computer)
+
+    args = parser.parse_args()
+
+    get_last_boot_time(args.computername)
+          
